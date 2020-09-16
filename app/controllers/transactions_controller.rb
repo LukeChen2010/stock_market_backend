0c87d7d268edc1b6f11ab4d5234bdbd82231a080
Luke Chen <luke.chen.2010@gmail.com>
2020-09-15 20:05:55 -0500

    CORS setup

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,4 +6,32 @@ class TransactionsController < ApplicationController
         render json: transactions
     end
 
+    def new
+        puts "test"
+
+        user_id = params[:user_id]
+        symbol = params[:symbol]
+        total_shares = params[:total_shares]
+        total_price = params[:total_price]
+        is_sell = params[:is_sell]
+
+        puts user_id
+        puts symbol
+        puts total_shares
+        puts total_price
+        puts is_sell
+
+        user = User.find_by(id: params[:user_id]) 
+
+        stock = Transaction.create(user_id: user_id, symbol: symbol, total_shares: total_shares, total_price: total_price, is_sell: is_sell)
+
+        render json: stock
+
+        if is_sell
+
+        else
+
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/:id/transactions' => 'transactions#index'
+  post 'users/:id/transactions/new' => 'transactions#new'
   get 'users/:id/stocks' => 'stocks#index'
   get 'stock_quote/:symbol' => 'static#stock_quote'
 end
