40200aa28307a23fc2eeefcd34462ea464df2e7f
Luke Chen <luke.chen.2010@gmail.com>
2020-09-21 21:07:33 -0500

    Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,3 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(balance: 100000)
-Transaction.create(user_id: 1, symbol: "SBUX", total_shares: 10, total_price: 860, is_sell: false)
-Transaction.create(user_id: 1, symbol: "AAPL", total_shares: 10, total_price: 1150, is_sell: false)
-Transaction.create(user_id: 1, symbol: "TSLA", total_shares: 10, total_price: 4200, is_sell: false)
-
-Transaction.create(user_id: 1, symbol: "SBUX", total_shares: 5, total_price: 400, is_sell: true)
-Transaction.create(user_id: 1, symbol: "AAPL", total_shares: 5, total_price: 600, is_sell: true)
-Transaction.create(user_id: 1, symbol: "TSLA", total_shares: 5, total_price: 2200, is_sell: true)
